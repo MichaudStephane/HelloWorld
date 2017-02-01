e094a7b1e4c6aceb2d88f784affb387b46bbc4a9
StéphaneMichaud <michaudstephane@yahoo.ca>
2017-02-01 10:28:09 -0500

    sick commentaires

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //make memes not drugs kids
             Console.WriteLine("HelloWorld");
         }
     }
